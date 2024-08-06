011083328c15dc773faf6c8c6e2d9058e8516953
Alexslec <mateus12bernardo@gmail.com>
2024-08-06 15:30:00 -0300

    Adição de Sprites

--- assets/scripts/level.py
+++ assets/scripts/level.py
@@ -14,12 +14,15 @@ class Level():
         self.visible_sprites = YSortCameraGroup()
         self.collision_sprites = pygame.sprite.Group()
         self.collectibles_sprites = pygame.sprite.Group()
+        self.collectibles_stars = pygame.sprite.Group()
+        self.collectibles_nebulae = pygame.sprite.Group()
+        self.collectibles_bombs = pygame.sprite.Group()
 
         self.screen = screen
 
         self.create_map(two=False)
 
-        self.music = pygame.mixer.Sound('assets/audio/firelink.wav')
+        self.music = pygame.mixer.Sound('assets/audio/ds.wav')
         self.music.set_volume(0.1)
         self.music.play(-1)
 
@@ -52,11 +55,57 @@ class Level():
                             surface = graphics['objects'][int(col)-2]
                             Tile((x, y), [self.collision_sprites, self.visible_sprites], 'object', surface)
 
-        Collectible((32, 32), [self.visible_sprites, self.collectibles_sprites], 'collectible', pygame.image.load('./assets/icons/Stars.png'))
-        if two:
-            self.player = Player((PLAYER_START_X+32, PLAYER_START_Y+32), [self.visible_sprites], self.collision_sprites, self.collectibles_sprites, self.screen, 2)
+        #Star:
+        Collectible((32, 384), [self.visible_sprites, self.collectibles_sprites, self.collectibles_stars], 'collectible', pygame.image.load('./assets/icons/Stars.png'))
+        #Bombs_wall:
+        Collectible((32, 352), [self.visible_sprites, self.collectibles_sprites, self.collectibles_bombs], 'collectible', pygame.image.load('./assets/icons/Bomb.png'))
+        Collectible((64, 384), [self.visible_sprites, self.collectibles_sprites, self.collectibles_bombs], 'collectible', pygame.image.load('./assets/icons/Bomb.png'))
+        Collectible((32, 416 ), [self.visible_sprites, self.collectibles_sprites, self.collectibles_bombs], 'collectible', pygame.image.load('./assets/icons/Bomb.png'))
+        #Star:
+        Collectible((224, 160), [self.visible_sprites, self.collectibles_sprites, self.collectibles_stars], 'collectible', pygame.image.load('./assets/icons/Stars.png'))
+        #Bombs_wall:
+        Collectible((224, 128), [self.visible_sprites, self.collectibles_sprites, self.collectibles_bombs], 'collectible', pygame.image.load('./assets/icons/Bomb.png'))
+        Collectible((256, 128), [self.visible_sprites, self.collectibles_sprites, self.collectibles_bombs], 'collectible', pygame.image.load('./assets/icons/Bomb.png'))
+        Collectible((256, 160), [self.visible_sprites, self.collectibles_sprites, self.collectibles_bombs], 'collectible', pygame.image.load('./assets/icons/Bomb.png'))
+        #Star:
+        Collectible((320, 576), [self.visible_sprites, self.collectibles_sprites, self.collectibles_stars], 'collectible', pygame.image.load('./assets/icons/Stars.png'))
+        #Bombs_wall:
+        Collectible((384, 576), [self.visible_sprites, self.collectibles_sprites, self.collectibles_bombs], 'collectible', pygame.image.load('./assets/icons/Bomb.png'))
+        Collectible((384, 544), [self.visible_sprites, self.collectibles_sprites, self.collectibles_bombs], 'collectible', pygame.image.load('./assets/icons/Bomb.png'))
+        Collectible((288, 576), [self.visible_sprites, self.collectibles_sprites, self.collectibles_bombs], 'collectible', pygame.image.load('./assets/icons/Bomb.png'))
+        Collectible((288, 544), [self.visible_sprites, self.collectibles_sprites, self.collectibles_bombs], 'collectible', pygame.image.load('./assets/icons/Bomb.png'))
+        Collectible((320, 544), [self.visible_sprites, self.collectibles_sprites, self.collectibles_bombs], 'collectible', pygame.image.load('./assets/icons/Bomb.png'))
+        Collectible((352, 544), [self.visible_sprites, self.collectibles_sprites, self.collectibles_bombs], 'collectible', pygame.image.load('./assets/icons/Bomb.png'))
+        #Star:
+        Collectible((736, 320), [self.visible_sprites, self.collectibles_sprites, self.collectibles_stars], 'collectible', pygame.image.load('./assets/icons/Stars.png'))
+        #Bombs_wall:
+        Collectible((736, 352), [self.visible_sprites, self.collectibles_sprites, self.collectibles_bombs], 'collectible', pygame.image.load('./assets/icons/Bomb.png'))
+        #Star:
+        Collectible((1120, 480), [self.visible_sprites, self.collectibles_sprites, self.collectibles_stars], 'collectible', pygame.image.load('./assets/icons/Stars.png'))
+        #Bombs_wall:
+        Collectible((1088, 480), [self.visible_sprites, self.collectibles_sprites, self.collectibles_bombs], 'collectible', pygame.image.load('./assets/icons/Bomb.png'))
 
-        self.player = Player((PLAYER_START_X, PLAYER_START_Y), [self.visible_sprites], self.collision_sprites, self.collectibles_sprites, self.screen, 1)
+        #Nebulae:
+        Collectible((32, 160), [self.visible_sprites, self.collectibles_sprites, self.collectibles_nebulae], 'collectible', pygame.image.load('./assets/icons/Nebulae.png'))
+        Collectible((288, 352), [self.visible_sprites, self.collectibles_sprites, self.collectibles_nebulae], 'collectible', pygame.image.load('./assets/icons/Nebulae.png'))
+        Collectible((352, 576), [self.visible_sprites, self.collectibles_sprites, self.collectibles_nebulae], 'collectible', pygame.image.load('./assets/icons/Nebulae.png'))
+        Collectible((736, 576), [self.visible_sprites, self.collectibles_sprites, self.collectibles_nebulae], 'collectible', pygame.image.load('./assets/icons/Nebulae.png'))
+
+
+        #Other Bombs:
+        Collectible((160, 256), [self.visible_sprites, self.collectibles_sprites, self.collectibles_bombs], 'collectible', pygame.image.load('./assets/icons/Bomb.png'))
+        Collectible((448, 128), [self.visible_sprites, self.collectibles_sprites, self.collectibles_bombs], 'collectible', pygame.image.load('./assets/icons/Bomb.png'))
+        Collectible((448, 352), [self.visible_sprites, self.collectibles_sprites, self.collectibles_bombs], 'collectible', pygame.image.load('./assets/icons/Bomb.png'))
+        Collectible((608, 288), [self.visible_sprites, self.collectibles_sprites, self.collectibles_bombs], 'collectible', pygame.image.load('./assets/icons/Bomb.png'))
+        Collectible((704, 576), [self.visible_sprites, self.collectibles_sprites, self.collectibles_bombs], 'collectible', pygame.image.load('./assets/icons/Bomb.png'))
+        Collectible((768, 576), [self.visible_sprites, self.collectibles_sprites, self.collectibles_bombs], 'collectible', pygame.image.load('./assets/icons/Bomb.png'))
+        Collectible((800, 800), [self.visible_sprites, self.collectibles_sprites, self.collectibles_bombs], 'collectible', pygame.image.load('./assets/icons/Bomb.png'))
+        Collectible((800, 320), [self.visible_sprites, self.collectibles_sprites, self.collectibles_bombs], 'collectible', pygame.image.load('./assets/icons/Bomb.png'))
+    
+        if two:
+            self.player = Player((PLAYER_START_X+32, PLAYER_START_Y+32), [self.visible_sprites], self.collision_sprites, self.collectibles_sprites, self.collectibles_stars, self.collectibles_nebulae, self.collectibles_bombs, self.screen, 2)
+
+        self.player = Player((PLAYER_START_X, PLAYER_START_Y), [self.visible_sprites], self.collision_sprites, self.collectibles_sprites, self.collectibles_stars, self.collectibles_nebulae, self.collectibles_bombs, self.screen, 1)
 
     def run(self):
         self.visible_sprites.custom_draw(self.player)
@@ -83,4 +132,6 @@ class YSortCameraGroup(pygame.sprite.Group):
         for sprite in sorted(self.sprites(), key = lambda sprite: sprite.rect.centery):
             offset_pos = sprite.rect.topleft - self.offset
             self.display_surface.blit(sprite.image, offset_pos)
+
+
 
